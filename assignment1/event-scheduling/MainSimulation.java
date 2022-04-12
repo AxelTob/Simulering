8ae14d5133159788b6effbb39b9436ee04c907f7
nikvogel <74900005+nikvogel@users.noreply.github.com>
2022-04-12 13:39:41 +0200

    Complete Task 1

--- assignment1/event-scheduling/MainSimulation.java
+++ assignment1/event-scheduling/MainSimulation.java
@@ -8,17 +8,20 @@ public class MainSimulation extends GlobalSimulation{
     	Event actEvent;
     	State actState = new State(); // The state that shoud be used
     	// Some events must be put in the event list at the beginning
-        insertEvent(ARRIVAL, 0);  
+        insertEvent(ARRIVAL1, 2);  
         insertEvent(MEASURE, 5);
-        
         // The main simulation loop
-    	while (time < 5000){
+    	while (time < 500000){
     		actEvent = eventList.fetchEvent();
     		time = actEvent.eventTime;
     		actState.treatEvent(actEvent);
     	}
     	
     	// Printing the result of the simulation, in this case a mean value
-    	System.out.println(1.0*actState.accumulated/actState.noMeasurements);
+    	System.out.println("Arrivals: " + actState.numberOfArrivals);
+    	System.out.println("Accumulated Queue 1: " + actState.accumulatedQueue1);
+    	System.out.println("Accumulated Queue 2: " + actState.accumulatedQueue2);
+    	System.out.println("Rejected: " + actState.numberOfRejected);
+    	System.out.println("Measurements: " + actState.numberMeasurements);
     }
 }
